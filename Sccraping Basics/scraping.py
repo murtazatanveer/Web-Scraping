fb8c25e472209a5d94839db61dabaab48c609bef
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2025-10-20 21:39:53 +0500

    Web Scraping.

---
+++ Sccraping Basics/scraping.py
@@ -0,0 +1,36 @@
+import requests;
+from bs4 import BeautifulSoup;
+import urllib3;
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+web = requests.get("https://www.comsats.edu.pk/",verify=False);
+
+# print(web.url);
+# print(web);
+# print(web.text);
+
+soup = BeautifulSoup(web.text,"html.parser");
+
+# print(soup.title);
+# print(soup.prettify());
+# print(soup.title.string);
+
+# Tag
+
+# print(soup.h1);
+# print(soup.h2.string);
+# print(soup.a);
+# print(soup.p);
+# print(soup.header);
+# print(soup.footer);
+# print(soup.div);
+# print(soup.span);
+# print(soup.img);
+# print(soup.section);
+
+with open("index.html", "w", encoding="utf-8") as file:
+    file.write(soup.prettify());
+
+
+
